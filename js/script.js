1265eaf67ae3d2eac20a2d39e3f345eef309704f
norizzz <fantomrizold@gmail.com>
2023-11-09 10:16:46 +0600

    half animation

---
+++ js/script.js
@@ -0,0 +1,52 @@
+
+window.addEventListener("load", () =>{
+    AOS.init({
+        duration: 1200,
+        easing:'ease-in-out-cubic'
+        
+    });
+})
+
+
+function headerMenu() {
+    const toggler = document.querySelector(".js-header-toggler");
+    const menu = document.querySelector(".js-header-menu");
+    const items = menu.querySelectorAll("li");
+
+    const menuToggle = () => {
+        menu.classList.toggle("open");
+        toggler.classList.toggle("active")
+    }
+    toggler.addEventListener("click", menuToggle);
+
+    items.forEach((item) => {
+        item.querySelector("a").addEventListener("click", () => {
+            if(window.innerWidth <= 991){
+                menuToggle();
+            }
+        });
+    });
+}
+headerMenu();
+
+
+
+function scheduleTabs(){
+    const tabs = document.querySelectorAll(".js-schedule-tab");
+
+    tabs.forEach((tab) => {
+        tab.addEventListener("click", () => {
+            if (tab.classList.contains("active")){
+                return;
+            }
+            document.querySelector(".js-schedule-tab.active").classList.remove("active");
+            tab.classList.add("active");
+            const target = tab.getAttribute("data-target");
+            document.querySelector(".js-schedule-table.active").classList.remove("active");
+            document.querySelector(target).classList.add("active");
+        });
+    });
+}
+scheduleTabs();
+
+Splitting();
